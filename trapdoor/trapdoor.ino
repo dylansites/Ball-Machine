95de96d0089f0383af0f01350fe691b3a7987dec
Dylan Sites <dws280@nyu.edu>
2013-05-05 19:07:42 -0400

    removed button functionality temporarily, added serial communication, checking 0, 90, and 180 degree angles for placement

--- trapdoor/trapdoor.ino
+++ trapdoor/trapdoor.ino
@@ -1,19 +1,20 @@
 #include <Servo.h>
 
 const int servoPin = 5;
-const int switchPin = 7;
+//const int switchPin = 7;
 const int servoPause = 100;
 
 Servo toyServo;
 
 void setup(){
-  pinMode(switchPin, INPUT);
+  Serial.begin(9600);
+ // pinMode(switchPin, INPUT);
   toyServo.attach(servoPin);
 }
 
 void loop(){
-  int switchVal = digitalRead(switchPin);
-  if(switchVal == HIGH){  
+  //int switchVal = digitalRead(switchPin);
+ /* if(switchVal == HIGH){  
     delay(servoPause);
     toyServo.write(180);
     delay(servoPause);
@@ -22,7 +23,15 @@ void loop(){
    delay(servoPause);
   toyServo.write(90);
   delay(servoPause); 
-  }
+  }*/
+  delay(500);
+  Serial.println("I am at 0");
+  toyServo.write(0);
+  delay(500);
+  Serial.println("I am at 90");
+  toyServo.write(90);
+  delay(500);
+  Serial.println("I am at 180");
+  toyServo.write(180);
+  delay(500);
 }
-
-/*can i push this*/
